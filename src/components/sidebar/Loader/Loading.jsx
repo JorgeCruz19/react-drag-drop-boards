105758e84f1ee5b6a2387e4763670144c9463e07
JorgeCruz19 <jorgecsibrian@gmail.com>
2022-06-08 14:02:56 -0600

    Implement Lazy loading and loader component

---
+++ src/components/sidebar/Loader/Loading.jsx
@@ -0,0 +1,9 @@
+import './loading.css'
+
+const Loading = () => {
+  return (
+    <span className="loader"></span>
+  )
+}
+
+export default Loading
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import Home from "../pages/Home/Home";
+
+import Loading from "../components/sidebar/Loader/Loading";
+import Dashboard from "../pages/Dashboard/Dashboard"
+const Home = lazy(() => import("../pages/Home/Home"));
+/* const Dashboard = lazy(() => import("../pages/Dashboard/Dashboard")); */
 
 const AppRouter = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Dashboard />}>
-					<Route path="/kanban" element={<Home />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route
+            path="/kanban"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Home />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
