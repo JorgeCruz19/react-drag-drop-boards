ebdc3756f1beb6c8cc8ce26ca8a0144ebfad0200
JorgeCruz19 <jorgecsibrian@gmail.com>
2022-06-12 19:43:16 -0600

    Add animation modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,29 @@
 import { useRef } from "react";
 import { unmountComponentAtNode } from "react-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import { MdOutlineClose } from "react-icons/md";
 import "./modal.css";
 
+const dropIn = {
+	hidden: {
+		opacity: 0,
+		transform: "scale(0.9)",
+	},
+	visible: {
+		transform: "scale(1)",
+		opacity: 1,
+		transition: {
+			duration: 0.1,
+			type: "spring",
+			damping: 25,
+			stiffness: 500,
+		},
+	},
+	exit: {
+		transform: "scale(0.9)",
+		opacity: 0,
+	},
+};
 const Modal = ({ children, title, root }) => {
 	const ref = useRef(null);
 	const handleCloseModal = () => {
@@ -11,17 +32,31 @@ const Modal = ({ children, title, root }) => {
 	};
 
 	return (
-		<div ref={ref} className="modal-container">
-			<div className="modal-inner-container">
-				<div className="modal-header">
-					<h3 className="modal-title">{title}</h3>
-					<button onClick={handleCloseModal} className="modal-close">
-						<MdOutlineClose />
-					</button>
-				</div>
-				<div className="modal-content">{children}</div>
-			</div>
-		</div>
+		<AnimatePresence>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				ref={ref}
+				className="modal-container"
+			>
+				<motion.div
+					variants={dropIn}
+					initial="hidden"
+					animate="visible"
+					exit="exit"
+					className="modal-inner-container"
+				>
+					<div className="modal-header">
+						<h3 className="modal-title">{title}</h3>
+						<button onClick={handleCloseModal} className="modal-close">
+							<MdOutlineClose />
+						</button>
+					</div>
+					<div className="modal-content">{children}</div>
+				</motion.div>
+			</motion.div>
+		</AnimatePresence>
 	);
 };
 
